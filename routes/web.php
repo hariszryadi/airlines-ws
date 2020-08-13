408dc503620fd31e73aea8bb9d1453866357c4d7
hariszryadi <harisz.ryadi17@gmail.com>
2020-08-13 10:32:25 +0700

    docker configuration

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-// $router->get('/', function () use ($router) {
-//     return $router->app->version();
-// });
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
 
 //auth
 $router->group(['prefix' => 'auth'], function() use ($router){
